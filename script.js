9d7d129f6f1d74a636b96ca5f0cee418fd0d4655
Maho Yutopia <indira.salcede@gmail.com>
2024-01-19 00:33:30 +0000

    adds time slot array

---
+++ script.js
@@ -0,0 +1,20 @@
+//display date top of the page
+
+// create time blocks + user input field
+
+var timeSlots = [
+  { time: "8 am", toDo: "" },
+  { time: "9 am", toDo: "" },
+  { time: "10 am", toDo: "" },
+  { time: "11 am", toDo: "" },
+  { time: "12 pm", toDo: "" },
+  { time: "1 pm", toDo: "" },
+  { time: "2 pm", toDo: "" },
+  { time: "3 pm", toDo: "" },
+  { time: "4 pm", toDo: "" },
+  { time: "5 pm", toDo: "" },
+  { time: "6 pm", toDo: "" },
+  { time: "7 pm", toDo: "" },
+];
+
+//save to local storage

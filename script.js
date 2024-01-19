6d7d7747b68d1438c0c9e0dc052a59c911e34e17
Maho Yutopia <indira.salcede@gmail.com>
2024-01-19 01:20:46 +0000

    adds date to page top + jqury ready

--- script.js
+++ script.js
@@ -2,20 +2,29 @@
 var currentDay = $("#currentDay");
 currentDay.append(dayjs().format("dddd, D MMMM YYYY"));
 
-// create time blocks for user input field
+//run when ready
+$(document).ready(function () {
+  // create time blocks
+  var timeSlots = [
+    "8.00",
+    "9.00",
+    "10.00",
+    "11.00",
+    "12.00",
+    "13.00",
+    "14.00",
+    "15.00",
+    "16.00",
+    "17.00",
+    "18.00",
+  ];
 
-var timeSlots = [
-  "8.00",
-  "9.00",
-  "10.00",
-  "11.00",
-  "12.00",
-  "13.00",
-  "14.00",
-  "15.00",
-  "16.00",
-  "17.00",
-  "18.00",
-];
+  //time slots containers
+  var container = $(".container");
 
-//save to local storage
+  //need for each loop to create blocks and input field
+
+  //save to local storage
+
+  //
+});
